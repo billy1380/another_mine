ce42f5c884297b247aed7e89737d74f6fa87a41a
William Shakour (billy1380) <william.shakour@gmail.com>
2023-10-19 14:21:14 +0100

    slightly cleaner code with filtering instead of double loops

--- lib/ai/guesser.dart
+++ lib/ai/guesser.dart
@@ -9,85 +9,60 @@ class Guesser {
 
   void makeAMove() {
     bool clicked = false;
-    int flagged, notPressed, lookingX, lookingY;
-    TileModel? tile;
+    int flagged, notPressed;
 
-    for (lookingY = 0; lookingY < game.state.difficulty.height; lookingY++) {
-      for (lookingX = 0; lookingX < game.state.difficulty.width; lookingX++) {
-        flagged = 0;
-        notPressed = 0;
+    List<TileModel> numbers = game.state.tiles
+        .where((t) => t.state.value > 0 && t.state.value <= 8)
+        .toList();
 
-        tile = game.state.tileAt(lookingX, lookingY);
+    for (TileModel tile in numbers) {
+      flagged = 0;
+      notPressed = 0;
 
-        if (tile != null) {
-          switch (tile.state) {
-            case TileStateType.one:
-            case TileStateType.two:
-            case TileStateType.three:
-            case TileStateType.four:
-            case TileStateType.five:
-            case TileStateType.six:
-            case TileStateType.seven:
-            case TileStateType.eight:
-              for (int i = 0; i < tile.neighbours.length; i++) {
-                if (tile.neighbours[i] != null) {
-                  final TileModel neighbour = tile.neighbours[i]!;
+      for (int i = 0; i < tile.neighbours.length; i++) {
+        if (tile.neighbours[i] != null) {
+          final TileModel neighbour = tile.neighbours[i]!;
 
-                  if (neighbour.state == TileStateType.notPressed ||
-                      neighbour.state == TileStateType.unsure) {
-                    notPressed++;
-                  } else if (neighbour.state ==
-                      TileStateType.predictedBombCorrect) {
-                    flagged++;
-                  }
-                }
-              }
-
-              if (TileStateType.from(notPressed + flagged) == tile.state) {
-                if (notPressed > 0) {
-                  for (int i = 0; i < tile.neighbours.length; i++) {
-                    if (tile.neighbours[i] != null) {
-                      final TileModel neighbour = tile.neighbours[i]!;
-
-                      if (neighbour.state !=
-                          TileStateType.predictedBombCorrect) {
-                        clicked = true;
-                        game.add(Speculate(model: neighbour));
-                      }
-                    }
-                  }
-
-                  break;
-                }
-              }
-
-              if (TileStateType.from(flagged) == tile.state && notPressed > 0) {
-                clicked = true;
-                game.add(Probe(model: tile));
-              }
-
-              break;
-            default:
-              break;
+          if (neighbour.state == TileStateType.notPressed ||
+              neighbour.state == TileStateType.unsure) {
+            notPressed++;
+          } else if (neighbour.state == TileStateType.predictedBombCorrect) {
+            flagged++;
           }
         }
+      }
 
-        if (clicked) break;
+      if (TileStateType.from(notPressed + flagged) == tile.state) {
+        if (notPressed > 0) {
+          for (int i = 0; i < tile.neighbours.length; i++) {
+            if (tile.neighbours[i] != null) {
+              final TileModel neighbour = tile.neighbours[i]!;
+
+              if (neighbour.state != TileStateType.predictedBombCorrect) {
+                clicked = true;
+                game.add(Speculate(model: neighbour));
+              }
+            }
+          }
+
+          break;
+        }
+      }
+
+      if (TileStateType.from(flagged) == tile.state && notPressed > 0) {
+        clicked = true;
+        game.add(Probe(model: tile));
       }
 
       if (clicked) break;
     }
 
-    if (game.state.isNotFinished) {
-      while (!clicked) {
-        tile = game.state.tileAt(lookingX = random(game.state.difficulty.width),
-            lookingY = random(game.state.difficulty.height));
-
-        if (tile?.state == TileStateType.notPressed) {
-          clicked = true;
-          game.add(Probe(model: tile!));
-        }
-      }
+    if (game.state.isNotFinished && !clicked) {
+      List<TileModel> remaining = game.state.tiles
+          .where((t) => t.state == TileStateType.notPressed)
+          .toList();
+      clicked = true;
+      game.add(Probe(model: remaining[random(remaining.length)]));
     }
   }
 }
